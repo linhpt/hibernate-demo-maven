0ca72720f09987c5263fab3da830deec0ed8a2a7
LinhPT <linh.phamtuan2@gmail.com>
2020-07-23 23:28:28 +0700

    get list object from database

--- src/main/java/org/hibernatedemomaven/App.java
+++ src/main/java/org/hibernatedemomaven/App.java
@@ -1,7 +1,54 @@
 package org.hibernatedemomaven;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernatedemomaven.models.Employees;
+
+import java.util.List;
+
 public class App {
+
+    public static Employees buildEmployee() {
+        Employees employees = new Employees();
+        employees.setEmployeeNumber(1000L);
+        employees.setEmail("email");
+        employees.setExtension("extension");
+        employees.setFirstName("firstName");
+        employees.setLastName("lastName");
+        employees.setJobTitle("PM");
+        employees.setOfficeCode("OS5");
+        employees.setReportsTo(1000L);
+        return employees;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Employees.class)
+                .configure("hibernate.cfg.xml");
+        ServiceRegistryBuilder registry = new ServiceRegistryBuilder();
+        registry.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = registry.buildServiceRegistry();
+
+        SessionFactory sf = configuration.buildSessionFactory(serviceRegistry);
+
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria crit = session.createCriteria(Employees.class);
+//        Employees employees = buildEmployee();
+//        session.save(employees);
+        List<Employees> employeesList = crit.list();
+        for (Employees employees : employeesList) {
+            System.out.println(employees);
+        }
+
+        transaction.commit();
+        session.getTransaction().commit();
+        session.close();
+//        System.out.println("End program");
+        System.out.println("End program " + employeesList.size());
     }
 }
---
+++ src/main/java/org/hibernatedemomaven/models/Employees.java
@@ -0,0 +1,83 @@
+package org.hibernatedemomaven.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employees {
+    @Id
+    private Long employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String extension;
+    private String email;
+    private String officeCode;
+    private Long reportsTo;
+    private String jobTitle;
+
+    public Long getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Long employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOfficeCode() {
+        return officeCode;
+    }
+
+    public void setOfficeCode(String officeCode) {
+        this.officeCode = officeCode;
+    }
+
+    public Long getReportsTo() {
+        return reportsTo;
+    }
+
+    public void setReportsTo(Long reportsTo) {
+        this.reportsTo = reportsTo;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
